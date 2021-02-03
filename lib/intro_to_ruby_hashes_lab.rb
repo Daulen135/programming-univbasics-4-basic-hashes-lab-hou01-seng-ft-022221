9dd9c01608357dee4e4217ec39bb03e5fd41fd2b
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-03 18:37:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
 id_hash_generator={:id=>5}
 id_hash_generator[:id]=451
+id_hash_generator
 end
 
 =begin
